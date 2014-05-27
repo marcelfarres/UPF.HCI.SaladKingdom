132b7b6d8bbea2549448f52b039fd15fa71ed158
naulacambra <naulacambra@gmail.com>
2014-05-27 21:33:48 +0200

    Database classe created

---
+++ Nightfall-of-the-Final-Conquer/app/src/main/java/example/nightfall_of_the_final_conquer/app/util/NFCSQLiteHelper.java
@@ -0,0 +1,27 @@
+package example.nightfall_of_the_final_conquer.app.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by Arnau on 27/05/2014.
+ */
+public class NFCSQLiteHelper extends SQLiteOpenHelper {
+
+    public NFCSQLiteHelper(Context context, String name, CursorFactory cursorFactory, int version){
+        super(context, name, cursorFactory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int previousVersion, int newVersion){
+
+    }
+}
