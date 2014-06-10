e9822e83a5f21f4f938b55e578882289f27e2958
Rous92 <rouspm92@gmail.com>
2014-06-11 01:26:00 +0200

    Google Map API added
    Configuración y mapa de prueba añadido.

---
+++ NightfalloftheFinalConquer/Game/src/main/java/up/hci/nightfallofthefinalconquer/game/MapActivity.java
@@ -0,0 +1,19 @@
+package up.hci.nightfallofthefinalconquer.game;
+
+/**
+ * Created by Roser on 11/06/2014.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import up.hci.nightfallofthefinalconquer.game.R;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
